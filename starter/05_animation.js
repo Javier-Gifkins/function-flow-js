e90d8fd0098c757ee626bb2f76ad08b7b91902fd
timjkue <timjkue@gmail.com>
2025-04-07 10:46:08 +1200

    changes

--- starter/05_animation.js
+++ starter/05_animation.js
@@ -2,7 +2,13 @@
 // TODO: Use 'console.clear()', 'console.log()', and basic logic with position
 
 function move() {
-  // Your code here
+  for (let i = 0; i < 100; i++) {
+    console.clear();
+    console.log(" ".repeat(i) + "🚀");
+    console.log(" ".repeat(100 - i) + "🚀");
+  }
 }
+
+move();
 
 module.exports = { move };
