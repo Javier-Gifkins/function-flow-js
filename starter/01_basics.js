4a3e8ffefd22445819e98d4ae7f2b57fc5d1ca81
timjkue <timjkue@gmail.com>
2025-04-07 10:01:40 +1200

    Completed activity 01

--- starter/01_basics.js
+++ starter/01_basics.js
@@ -1,8 +1,17 @@
 // TODO: Create a student object with 'name' and 'age' properties
 // TODO: Write a function 'printStudentName' that logs the student's name
 
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
 function printStudentName() {
-  // Your code here
+  student = new Student("John", 20);
+  console.log(student.name);
+  return student.name;
 }
 
 module.exports = { printStudentName };
